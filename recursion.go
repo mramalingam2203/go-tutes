4153fac0090562949e3c78064957604e2523a6b8
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-19 10:44:16 +0530

    raised to the power of ..

--- recursion.go
+++ recursion.go
@@ -12,7 +12,14 @@ func main() {
 	inputArray := []int{1, 2, 3, 4, 5}
 	fmt.Println(recursiveSum(inputArray, 0))
 
+	fmt.Println(recursivePower(2, 5))
+
 }
+
+/*
+Factorial Calculation:
+Write a recursive function to calculate the factorial of a given positive integer n.
+*/
 
 func factorial(n int) int {
 
@@ -22,6 +29,11 @@ func factorial(n int) int {
 
 	return n * factorial(n-1)
 }
+
+/*
+Fibonacci Series:
+Implement a recursive function to generate the Fibonacci sequence up to the nth term.
+*/
 
 func Fibonacci(n int) int {
 
@@ -35,6 +47,11 @@ func Fibonacci(n int) int {
 
 }
 
+/*
+Sum of Array Elements:
+Write a recursive function to calculate the sum of all elements in an array.
+*/
+
 func recursiveSum(array []int, startIndex int) int {
 
 	// Base case: If the array is empty, return 0
@@ -45,4 +62,21 @@ func recursiveSum(array []int, startIndex int) int {
 	currentElement := array[startIndex]
 	restOfArraySum := recursiveSum(array, startIndex+1)
 	return currentElement + restOfArraySum
+}
+
+/*
+
+Power Calculation:
+Implement a recursive function to calculate the result of raising a number x to the power of a non-negative integer n.
+
+*/
+func recursivePower(x, n int) int {
+
+	if n == 0 {
+		return 1
+	} else if n == 1 {
+		return x
+	}
+
+	return x * recursivePower(x, n-1)
 }
